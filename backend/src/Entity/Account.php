18d54527bdf7885ddb785ecc75ae5dc5d7a81119
Romans Antonovics <romans.antonovics@gmail.com>
2025-08-29 09:22:43 +0300

    feat(api): import Symfony Validator (Assert) and enable Account constraints

--- backend/src/Entity/Account.php
+++ backend/src/Entity/Account.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AccountRepository::class)]
 #[ApiResource]
@@ -18,12 +19,27 @@ class Account
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Name is required")]
+    #[Assert\Length(max: 255, maxMessage: "Name must be at most 255 characters")]
     private ?string $name = null;
 
     #[ORM\Column(type: 'float')]
+    #[Assert\NotNull(message: "Balance is required")]
+    #[Assert\Type(type: 'numeric', message: "Balance must be a number")]
+    #[Assert\GreaterThanOrEqual(value: 0, message: "Balance cannot be negative")]
     private ?float $balance = null;
 
     #[ORM\Column(length: 3)]
+    #[Assert\NotBlank(message: "Currency is required")]
+    #[Assert\Length(
+        min: 3,
+        max: 3,
+        exactMessage: "Currency must be exactly 3 letters (ISO 4217)"
+    )]
+    #[Assert\Regex(
+        pattern: "/^[A-Z]{3}$/",
+        message: "Currency must be uppercase ISO code (e.g. EUR, USD)"
+    )]
     private ?string $currency = null;
 
     #[ORM\OneToMany(mappedBy: 'account', targetEntity: Transaction::class, orphanRemoval: true)]
